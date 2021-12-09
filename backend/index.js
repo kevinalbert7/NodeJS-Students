14e9fe4eb69191cf2a7b6b52df7cf62ba1da5257
kevinalbert7 <20ko@live.fr>
2021-12-09 10:14:12 +0100

    fin de programmation du backend

--- backend/index.js
+++ backend/index.js
@@ -12,16 +12,22 @@ app.use(bodyparser.urlencoded({ extended: true }))
 
 
 // import de la route
-const students = require("./students.json")
+let students = require("./students.json")
 console.log(students)
 
 app.get('/students', (req, res) => {
     res.json(students)
 })
 
-// app.post('/students', (req, res) => {
+app.post('/students', (req, res) => {
+    const student = {
+        ...req.body,
+        id: students.length + 1
+      }
     
-// })
+      students = [...students, student]
+      res.json(student)
+})
 
 app.listen(port, () => {
     console.log(`running start server at port ${port}`)
