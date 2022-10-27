dc68e177660758be6bd2816d627916fd45c0ede6
kevinalbert7 <20ko@live.fr>
2022-10-27 12:15:24 +0200

    Folder Routes created

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,16 @@
 const express = require("express")
 const app = express()
-const cors = require('cors')
-const bodyparser = require("body-parser")
+// const cors = require('cors')
 const port = 5000
 
+const students = require("./routes/students")
 
-app.use(cors())
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: true }))
+// app.use(cors())
+app.use(express.json())
 
+app.use('/students', students)
 
-
-// import de la route
-let students = require("./students.json")
-console.log(students)
-
-app.get('/students', (req, res) => {
-    res.json(students)
-})
-
-app.post('/students', (req, res) => {
-    const student = {
-        ...req.body,
-        id: students.length + 1
-      }
-    
-      students = [...students, student]
-      res.json(student)
-})
 
 app.listen(port, () => {
-    console.log(`running start server at port ${port}`)
+    console.log(`Server running on port ${port}`)
 })
---
+++ backend/routes/students.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+
+let students = require("../students.json")
+
+app.get("/", (req, res) => {
+    res.json(students)
+})
+
+app.post("/", (req, res) => {
+    const student = {
+        ...req.body,
+        id: students.length + 1
+      }
+    
+      students = [ ...students, student ]
+      res.json(student)
+})
+
+module.exports = app
