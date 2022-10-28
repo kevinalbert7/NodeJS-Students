e31bb7be2952cbf74f6df24ce6fc28f719af8d28
kevinalbert7 <20ko@live.fr>
2022-10-28 13:45:51 +0200

    Backend finish

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const app = express()
-// const cors = require('cors')
+const cors = require('cors')
 const port = 5000
 
 const students = require("./routes/students")
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 app.use('/students', students)
